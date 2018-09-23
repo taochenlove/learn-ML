ff1ae63b866a28d4cee1b25f9c7100ede86571b0
taochenlove <chen_tao_love@yahoo.com>
2018-09-23 16:50:09 +0800

    update

--- code/1day.py
+++ code/1day.py
@@ -42,6 +42,11 @@ labelencoder_Y = LabelEncoder()
 Y = labelencoder_Y.fit_transform(Y)
 
 from sklearn.model_selection import train_test_split
+print("=========>>X<<===============")
+print(X)
+print("=========>>Y<<===============")
+print(Y)
+
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
 
 print("X_train")
